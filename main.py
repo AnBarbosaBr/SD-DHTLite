5072e0de9492f1dbdee0a753016aec514e022f7c
Otazz <Otazz@users.noreply.github.com>
2018-08-22 10:18:23 -0300

    Add operations on memory

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 from flask import Flask, request, render_template
 from jinja2 import Template
+import sys
 
 app = Flask(__name__)
 
 default_address = '0.0.0.0/9000'
 
+usuarios = {}
+
 @app.route("/", methods=['GET'])
 def root():
 	return render_template('home.html', conectado=True, 
-																			enderco_next_node="",
-																			enderco_previous_node="")
+																			endereco_next_node="ds",
+																			endereco_previous_node="da")
 
 @app.route("/connect", methods=['GET', 'POST'])
 def connect():
 	if request.method == 'POST':
-		ip = request.form['ip']
-		port = request.form['port']
-		print(ip, port)
-		#conectar com o node do ip e port indicados
-	#else
-		#conectar com o endereço default
+		try:
+			ip = request.form['ip']
+			port = request.form['port']
+			print(ip, port)
+			s_m = "Conectado!"
 
-	return "Conectado"
+			#conectar com o node do ip e port indicados
+			#else
+			#conectar com o endereço default
+			return render_template('sucess.html', success_message=s_m)
+
+		except Exception as err:
+		 return render_template('failure.html', failure_message=str(err))
+		
 
 @app.route("/dc", methods=['GET'])
 def dc():
@@ -30,27 +39,53 @@ def dc():
 
 @app.route("/add", methods=['GET','POST'])
 def add():
-	username = request.form['usuario']
-	name = request.form['nome']
-	email = request.form['email']
-	s_m = "{} adicionado com sucesso".format(username)
-	print(s_m)
+	try:
+		username = request.form['usuario']
+		name = request.form['nome']
+		email = request.form['email']
+		usuarios[username] = {
+			'nome' : name,
+			'email': email
+		}
+		print(usuarios)
+		s_m = "{} adicionado com sucesso".format(username)
 
-	#metodo de adicionar contato na rede
-	return render_template('sucess.html', sucess_message=s_m)
+
+		#metodo de adicionar contato na rede
+		return render_template('sucess.html', sucess_message=s_m)
+
+	except Exception as err:
+		 return render_template('failure.html', failure_message=str(err))
 
 @app.route("/delete", methods=['POST'])
 def delete():
-	username = request.form['username']
+	try:
+		username = request.form['usuario']
+		if username not in usuarios:
+			raise Exception("Nome de usuario não cadastrado")
+		del usuarios[username]
+		s_m = "{} apagado com sucesso".format(username)
+		#metodo de deletar contato da rede
+		return render_template('sucess.html', sucess_message=s_m)
+	except Exception as err:
+		 return render_template('failure.html', failure_message=str(err))
 
-	#metodo de deletar contato da rede
-	return "{} apagado com sucesso".format(username)
-
-@app.route("/search", methods=['GET'])
+@app.route("/search", methods=['POST'])
 def search():
-	username = request.form['username']
+	try:
+		username = request.form['usuario']
+		if username not in usuarios:
+			raise Exception("Nome de usuario não cadastrado")
+		name = usuarios[username]['nome']
+		email = usuarios[username]['email']
+
+		s_m = "Usuario: {},\nNome: {}\nEmail:{}".format(username, name, email)
+		return render_template('sucess.html', sucess_message=s_m)
+	except Exception as err:
+		 return render_template('failure.html', failure_message=str(err))
 
 	#metodo de buscar pelo username
+
 
 #@app.route("")
 
