93cd997598ba7288c15934a06b5fcf447381e37b
Otazz <Otazz@users.noreply.github.com>
2018-08-22 09:44:17 -0300

    Initial integration with views

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from jinja2 import Template
 
-app = Flask(__name__, static_url_path='static')
+app = Flask(__name__)
 
 default_address = '0.0.0.0/9000'
 
 @app.route("/", methods=['GET'])
 def root():
-	return app.send_static_file('index.html')
+	return render_template('home.html', conectado=True, 
+																			enderco_next_node="",
+																			enderco_previous_node="")
 
 @app.route("/connect", methods=['GET', 'POST'])
 def connect():
 	if request.method == 'POST':
 		ip = request.form['ip']
 		port = request.form['port']
+		print(ip, port)
 		#conectar com o node do ip e port indicados
-	else
+	#else
 		#conectar com o endereço default
 
 	return "Conectado"
 
-@app.route("/dc", method=['GET'])
+@app.route("/dc", methods=['GET'])
 def dc():
 	#metodo de desconectar do node
 	return "Desconectado"
 
-@app.route("/add", methods=['POST'])
+@app.route("/add", methods=['GET','POST'])
 def add():
-	username = request.form['username']
-	name = request.form['name']
+	username = request.form['usuario']
+	name = request.form['nome']
 	email = request.form['email']
+	s_m = "{} adicionado com sucesso".format(username)
+	print(s_m)
 
 	#metodo de adicionar contato na rede
-	return "{} adicionado com sucesso".format(username)
+	return render_template('sucess.html', sucess_message=s_m)
 
 @app.route("/delete", methods=['POST'])
 def delete():
@@ -46,7 +52,7 @@ def search():
 
 	#metodo de buscar pelo username
 
-@app.route("")
+#@app.route("")
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8080)
+	app.run(host='0.0.0.0', port=9000)
